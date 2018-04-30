5bf82554a13b659a8b436c1b5bce3faa08632a81
JoshuaBrigati <brigati.joshua@gmail.com>
2018-04-29 20:17:34 -0500

    made it where people have to sign in

--- src/client/controllers/goals-controller.js
+++ src/client/controllers/goals-controller.js
@@ -3,27 +3,31 @@ angular
   .controller("GoalsController", function($scope, $window, $location, Goals) {
     $scope.goals = [];
 
-    Goals.getGoals($window.localStorage.user_id).then(response => {
-      $scope.goals = response.data;
-    });
-
-    $scope.newgoal = () => {
-      $location.url("/goal-form");
-    };
-
-    $scope.renderDetails = goal => {
-      $location.url(`/goals/:${goal.id}`);
-    };
-
-    $scope.titleHome = () => {
-      $location.url(`/`);
-    };
-
-    $scope.signin = () => {
+    if (!!$window.localStorage.getItem("com.gp") === false) {
       $location.url("/signin");
-    };
+    } else {
+      Goals.getGoals($window.localStorage.user_id).then(response => {
+        $scope.goals = response.data;
+      });
 
-    $scope.isLoggedIn = () => {
-      return true;
-    };
+      $scope.newgoal = () => {
+        $location.url("/goal-form");
+      };
+
+      $scope.renderDetails = goal => {
+        $location.url(`/goals/:${goal.id}`);
+      };
+
+      $scope.titleHome = () => {
+        $location.url(`/`);
+      };
+
+      $scope.signin = () => {
+        $location.url("/signin");
+      };
+
+      $scope.isLoggedIn = () => {
+        return true;
+      };
+    }
   });
